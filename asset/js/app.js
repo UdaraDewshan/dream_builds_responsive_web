edaa96a0c38803cf4c64ef4fc614a3340b21f556
UdaraDewshan <dewudara@gmail.com>
2026-04-30 01:12:00 +0530

    Add About Us section with content and styles in index.html and style.css

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,3 +1,49 @@
+const mobileMenuBtn = document.getElementById('mobile-menu-btn');
+const navLinks = document.getElementById('nav-links');
+const header = document.getElementById('main-header');
+const navItems = document.querySelectorAll('.nav-links a');
+const sections = document.querySelectorAll('section');
+
+mobileMenuBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active');
+    });
+});
+
+
+window.addEventListener('scroll', () => {
+
+
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+
+    let current = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    });
+
+    navItems.forEach(a => {
+        a.classList.remove('active');
+        if (current && a.getAttribute('href').includes(current)) {
+            a.classList.add('active');
+        }
+    });
+});
+
+
 const modal = document.getElementById('project-modal');
 const closeBtn = document.querySelector('.close-btn');
 const modalImg = document.getElementById('modal-img');
@@ -10,9 +56,8 @@ const viewBtns = document.querySelectorAll('.card-link');
 viewBtns.forEach(btn => {
     btn.addEventListener('click', (e) => {
         e.preventDefault();
-        
+
         const card = btn.closest('.card');
-        
         const imgSrc = card.querySelector('.card-img').src;
         const title = card.querySelector('h3').innerText;
         const desc = card.querySelector('p').innerText;
